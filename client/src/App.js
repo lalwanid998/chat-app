dabecd298abce0f8706c698e29b65d6bd06ed863
lalwanid998 <lalwanid998@gmail.com>
2024-04-08 23:03:16 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Join from './components/Join/Join';
+import Chat from './components/Chat/Chat';
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+      <Route path="/" exact Component={Join} />
+      <Route path="/chat" Component={Chat} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App;
